fb140f061ce579ae75af987e9ae6b87dc8e6ff62
M RISQI HIDAYAT <79409216+rizkihdyt6@users.noreply.github.com>
2023-04-01 13:37:07 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+###########- COLOR CODE -##############
+REPO="https://raw.githubusercontent.com/rizkihdyt6/scupdate/rizki/"
+echo -e " [INFO] Downloading Update File"
+sleep 2
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkihdyt6/scupdate/rizki/config/menu.zip" && chmod +x /usr/bin/menu
+echo -e " [INFO] Update Successfully"
+sleep 2
+exit
