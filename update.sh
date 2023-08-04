28f40a26f0b7bab91eaf0da25f1ddd03bda30447
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-08-04 22:29:45 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
-REPO="https://raw.githubusercontent.com/rizkihdyt6/scupdate/rizki/"
+REPO="https://raw.githubusercontent.com/myvpn1/scupdate-1/rizki/"
 echo -e " [INFO] Downloading Update File"
 sleep 2
-wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/rizkihdyt6/scupdate/rizki/config/menu.zip" && chmod +x /usr/bin/menu
+wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/myvpn1/scupdate-1/rizki/config/menu.zip" && chmod +x /usr/bin/menu
 echo -e " [INFO] Update Successfully"
 sleep 2
 exit
